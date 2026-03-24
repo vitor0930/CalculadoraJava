c8e3a0aab1c6580f94ae894690e7704e5db147b1
wagnermarques <wagnerdocri@gmail.com>
2026-03-24 11:01:23 -0300

    Comeco das funcionalidades

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,52 +1,82 @@
 import javax.swing.*;
 import java.awt.*;
+
 public class Calculadora {
 
     private JFrame janela;
     private JTextField visorEquacao;
     private JTextField visorAtual;
 
-    public Calculadora(){
+    private double resultado = 0;
+
+    public Calculadora() {
         janela = new JFrame();
         visorEquacao = new JTextField();
         visorAtual = new JTextField("0");
-        
 
         janela.setSize(350, 450);
         janela.setTitle("Calculadora");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        JPanel painelVisores = new JPanel(new GridLayout(2,1));
-        
+
+        JPanel painelVisores = new JPanel(new GridLayout(2, 1));
+
         visorEquacao.setEditable(false);
         visorEquacao.setHorizontalAlignment(JTextField.RIGHT);
-        
+
         visorAtual.setEditable(false);
         visorAtual.setHorizontalAlignment(JTextField.RIGHT);
         visorAtual.setFont(new Font("Arial", Font.BOLD, 28));
-        
+
         painelVisores.add(visorEquacao);
         painelVisores.add(visorAtual);
-        
-        janela.add(painelVisores,BorderLayout.NORTH);
 
-        JPanel painelBotoes = new JPanel(new GridLayout(4, 4));
+        janela.add(painelVisores, BorderLayout.NORTH);
 
-        String[] textos = new String[]{ "7","8","9","/",
-                                        "4","5","6","*",
-                                        "1","2","3","-",
-                                        "C","0","=","+"
+        JPanel painelBotoes = new JPanel(new GridLayout(4, 4, 5, 5));
+
+        String[] textos = new String[] { 
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "C", "0", "=", "+"
         };
 
         for (String texto : textos) {
             JButton btn = new JButton(texto);
             painelBotoes.add(btn);
-            btn.addActionListener(null);
+            btn.addActionListener(e -> tratarClique(texto));
         }
 
-        janela.add(painelBotoes);
-        
+        janela.add(painelBotoes, BorderLayout.CENTER);
+
         janela.setVisible(true);
 
+    }
+
+    private void tratarClique(String s) {
+        calcular(Double.parseDouble(visorAtual.getText()), s);
+        if ("0123456789".contains(s)) {
+            visorAtual.setText(s);
+        } else if ("+-/*=".contains(s)) {
+            if (s.equals("=")) {
+                visorEquacao.setText(visorEquacao.getText() + visorAtual.getText() + " = ");
+                visorAtual.setText(String.valueOf(resultado));
+                resultado = 0;
+            } else {
+                visorEquacao.setText(visorEquacao.getText() + visorAtual.getText() + " " + s + " ");
+            }
+        }
+    }
+
+    private void calcular(double valor, String operador){
+        if (operador.equals("+")) {
+            resultado += valor;
+        } else if (operador.equals("-")){
+            resultado -= valor;
+        } else if (operador.equals("*")){
+            resultado *= valor;
+        } else if (operador.equals("*")){
+            resultado /= valor;
+        }
     }
 }
